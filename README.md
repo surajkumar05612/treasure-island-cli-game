1ba4285fd127e13ca42e5f5b509976144ac9761c
Suraj Kumar Jena <73339517+surajkumar05612@users.noreply.github.com>
2021-01-07 23:02:00 +0530

    updated

---
+++ README.md
@@ -0,0 +1,16 @@
+# Hello Everyone!!
+## TIP-CALCULATOR
+
+It's a simple python language based CLI game aka treasure island game. its created using conditional statements of python.
+
+Live link:-
+
+https://repl.it/@DevSuraj/treasure-island-start?embed=1&output=1
+
+Languages Used:-
+
+python
+
+Why I Made this?
+
+I have made this under 100 days of python challenge. Its my day 3 mini project.
---
+++ main.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+path = input("you're at crossroad where you want to go? Type 'left' or 'right' ")
+path = path.lower()
+if path == "right":
+  print("you have reached to a sea which will take you to treasure island what you want to do? wait for Boat to ride or want to swim?")
+  path_1 = input( "Type 'boat' or 'swim' ")
+  path_1 = path_1.lower()
+  if path_1 == "boat":
+    print("you reached to treasure island & you are at final door there are three doors which door you will enter?")
+    path_2 = input(" 'Yellow', 'Green', 'red' ")
+    path_2 = path_2.lower()
+    if path_2 == "green":
+      print("you got the treasure hunt successfully , congratulations")
+    else:
+      ("you entered to wrong room you are dead, Game Over")
+  else:
+    print("while swimming you got drown , you are dead, Game Over")
+else:
+  print("you were haunted by wild animal !! game over")
+  
